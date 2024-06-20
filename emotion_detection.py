9c7bf93678c37b57def3df7ce809b49199b1f816
SufBit <sufarshad.99@gmail.com>
2024-06-20 18:28:17 -0500

    emotion detection

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,5 @@
-from flask import Flask
 import requests
+import json
 
 
 def emotion_detector(text_to_analyse):
@@ -7,5 +7,27 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     data = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, headers=headers, json=data)
-    return response.json()
+
+    if response.status_code == 200:
+        json_data = response.json()
+        emotion_pred = json_data.get('emotionPredictions', [])
+        extracted_emotions = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None,
+        }
+
+        if emotion_pred:
+            ex_emotions = emotion_pred[0].get('emotion', {})
+            for i in extracted_emotions.keys():
+                if i in ex_emotions:
+                    extracted_emotions[i] = ex_emotions[i]
+
+            dominant_emotion = max(ex_emotions, key=ex_emotions.get)
+            extracted_emotions['dominant_emotion'] = dominant_emotion
+        
+        return extracted_emotions
 
